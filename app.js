9446039c40ad1bd9561e3c78879dcf8a7614aee6
sortixchecker <110255163+sortixchecker@users.noreply.github.com>
2026-02-15 22:30:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // CONFIG
 const BIN_ID = "699289be43b1c97be9824f7f";
-const MASTER_KEY = "$2a$10$BBRC77TElZJiVK11kI2LHeyCk.f1vAvTllgJEdYCGcOLlqPv51ICG";
+const MASTER_KEY = "$2a$10$1BxDo7tnxMVIX.NxfIofNuEM26ZXw1jykGJBD0dyFrcHg35H9zuaC";
 const BIN_URL = `https://api.jsonbin.io/v3/b/${BIN_ID}`;
 
 // ======================
@@ -104,3 +104,4 @@ export function download(fileName) {
   link.click();
   document.body.removeChild(link);
 }
+
